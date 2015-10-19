2c0d80be7d5f597074eeb8ef2719ff69a6ea92c4
marcodippy <dpl.marco@yahoo.it>
2015-10-19 18:30:03 +0100

    refactoring

--- hadoop/word_count/src/main/java/org/mdp/learn/hadoop/word_count/WordCountDriver.java
+++ hadoop/word_count/src/main/java/org/mdp/learn/hadoop/word_count/WordCountDriver.java
@@ -5,49 +5,28 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.mdp.learn.hadoop.commons.HdfsUtils;
+import org.mdp.learn.hadoop.commons.JobBuilder;
 
 public class WordCountDriver extends Configured implements Tool {
-	public int run(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.out.println("Usage: [input] [output]");
-			System.exit(-1);
-		}
+  public int run(String[] args) throws Exception {
+    Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
 
-		Job job = Job.getInstance(getConf());
-		job.setJobName("Word Count");
-		job.setJarByClass(WordCountDriver.class);
+    job.setMapperClass(WordCountMapper.class);
+    job.setCombinerClass(WordCountReducer.class);
+    job.setReducerClass(WordCountReducer.class);
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
 
-		job.setMapperClass(WordCountMapper.class);
-		job.setCombinerClass(WordCountReducer.class);
-		job.setReducerClass(WordCountReducer.class);
+    HdfsUtils.deleteIfExists(getConf(), new Path(args[1]));
 
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
 
-		Path inputFilePath = new Path(args[0]);
-		Path outputFilePath = new Path(args[1]);
-
-		FileInputFormat.addInputPath(job, inputFilePath);
-		FileOutputFormat.setOutputPath(job, outputFilePath);
-
-		HdfsUtils.deleteIfExists(getConf(), outputFilePath);
-
-		return job.waitForCompletion(true) ? 0 : 1;
-	}
-
-	public static void main(String[] args) throws Exception {
-		WordCountDriver wordcountDriver = new WordCountDriver();
-		int res = ToolRunner.run(wordcountDriver, args);
-		System.exit(res);
-	}
+  public static void main(String[] args) throws Exception {
+    System.exit(ToolRunner.run(new WordCountDriver(), args));
+  }
 }
