e8a7147af0960720761eb6b9c4accabfd0d4fdf6
marcodippy <dpl.marco@yahoo.it>
2015-10-20 19:44:09 +0100

    move constants

--- hadoop/average_grade/src/main/java/org/mdp/learn/hadoop/average_grade/AverageGradeJobConstants.java
+++ hadoop/average_grade/src/main/java/org/mdp/learn/hadoop/average_grade/AverageGradeJobConstants.java
@@ -2,4 +2,8 @@ package org.mdp.learn.hadoop.average_grade;
 
 public class AverageGradeJobConstants {
   public static final String OUTPUT_FIELD_SEPARATOR = ",";
+  public static final String INPUT_FIELD_SEPARATOR = ";";
+  public static final int COURSE_INDEX = 0;
+  public static final int STUDENT_INDEX = 1;
+  public static final int GRADE_INDEX = 2;
 }
--- hadoop/average_grade/src/main/java/org/mdp/learn/hadoop/average_grade/AverageGradeMapper.java
+++ hadoop/average_grade/src/main/java/org/mdp/learn/hadoop/average_grade/AverageGradeMapper.java
@@ -6,13 +6,9 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import static org.mdp.learn.hadoop.average_grade.AverageGradeJobConstants.*;
 
 public class AverageGradeMapper extends Mapper<LongWritable, Text, CourseAndStudentWritable, IntWritable> {
-  private static final String            INPUT_FIELD_SEPARATOR = ";";
-  private static final int               COURSE_INDEX          = 0;
-  private static final int               STUDENT_INDEX         = 1;
-  private static final int               GRADE_INDEX           = 2;
-
   private final CourseAndStudentWritable mapKey                = new CourseAndStudentWritable();
   private final IntWritable              grade                 = new IntWritable();
 
