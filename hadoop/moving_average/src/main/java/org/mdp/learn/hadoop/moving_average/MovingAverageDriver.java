f0471f22b5cf276cc3b527c0b632a8078a9a3474
Marco Di Paola <dpl.marco@gmail.com>
2015-10-22 17:49:16 +0100

    add Driver for the job

--- hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageDriver.java
+++ hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageDriver.java
@@ -1,5 +1,35 @@
 package org.mdp.learn.hadoop.moving_average;
 
-public class MovingAverageDriver {
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.mdp.learn.hadoop.commons.HdfsUtils;
+import org.mdp.learn.hadoop.commons.JobBuilder;
 
+public class MovingAverageDriver extends Configured implements Tool {
+  public int run(String[] args) throws Exception {
+    Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+
+    job.setMapperClass(MovingAverageMapper.class);
+    job.setPartitionerClass(MovingAverageKeyPartitioner.class);
+    job.setSortComparatorClass(MovingAverageKeyComparator.class);
+    job.setGroupingComparatorClass(MovingAverageKeyGroupingComparator.class);
+    job.setReducerClass(MovingAverageReducer.class);
+    
+    job.setMapOutputKeyClass(MovingAverageKey.class);
+    job.setMapOutputValueClass(FloatWritable.class);
+
+    job.getConfiguration().set("mapreduce.output.textoutputformat.separator", "");
+
+    HdfsUtils.deleteIfExists(getConf(), new Path(args[1]));
+
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    System.exit(ToolRunner.run(new MovingAverageDriver(), args));
+  }
 }
--- hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageReducer.java
+++ hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageReducer.java
@@ -1,5 +1,8 @@
 package org.mdp.learn.hadoop.moving_average;
 
-public class MovingAverageReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MovingAverageReducer extends Reducer<MovingAverageKey, Float, Text, Text> {
 
 }
