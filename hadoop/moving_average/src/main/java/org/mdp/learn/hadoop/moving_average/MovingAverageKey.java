74d3f88ae2f9a08ba0c8362399e672c3edbde31b
marcodippy <dpl.marco@yahoo.it>
2015-10-22 20:14:01 +0100

    start with moving average

--- hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageKey.java
+++ hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageKey.java
@@ -4,23 +4,84 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
 public class MovingAverageKey implements WritableComparable<MovingAverageKey> {
+  private Text departureAirport, arrivalAirport;
+  private LongWritable timestamp;
+
+  public MovingAverageKey() {
+    this.departureAirport = new Text();
+    this.arrivalAirport = new Text();
+    this.timestamp = new LongWritable();
+  }
+
+  public MovingAverageKey(String departureAirport, String arrivalAirport, Long timestamp) {
+    this.departureAirport = new Text(departureAirport);
+    this.arrivalAirport = new Text(arrivalAirport);
+    this.timestamp = new LongWritable(timestamp);
+  }
+
+  public Text getDepartureAirport() {
+    return departureAirport;
+  }
+
+  public Text getArrivalAirport() {
+    return arrivalAirport;
+  }
+
+  public LongWritable getTimestamp() {
+    return timestamp;
+  }
 
   @Override
   public void write(DataOutput out) throws IOException {
-    
+    departureAirport.write(out);
+    arrivalAirport.write(out);
+    timestamp.write(out);
   }
 
   @Override
   public void readFields(DataInput in) throws IOException {
-    
+    departureAirport.readFields(in);
+    arrivalAirport.readFields(in);
+    timestamp.readFields(in);
   }
 
   @Override
   public int compareTo(MovingAverageKey o) {
-    return 0;
+    int cmp = this.departureAirport.compareTo(o.departureAirport);
+
+    if (cmp == 0) {
+      cmp = this.arrivalAirport.compareTo(o.arrivalAirport);
+      if (cmp == 0)
+        cmp = this.timestamp.compareTo(o.timestamp);
+    }
+
+    return cmp;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((arrivalAirport == null) ? 0 : arrivalAirport.hashCode());
+    result = prime * result + ((departureAirport == null) ? 0 : departureAirport.hashCode());
+    result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof MovingAverageKey) {
+      MovingAverageKey mak = (MovingAverageKey) obj;
+      return departureAirport.equals(mak.departureAirport) && arrivalAirport.equals(mak.arrivalAirport)
+          && timestamp.equals(mak.timestamp);
+    }
+
+    return false;
   }
 
 }
--- hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageMapper.java
+++ hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageMapper.java
@@ -1,6 +1,8 @@
 package org.mdp.learn.hadoop.moving_average;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -11,7 +13,23 @@ public class MovingAverageMapper extends Mapper<LongWritable, Text, MovingAverag
 
   @Override
   protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-    
+    String[] values = value.toString().split(";");
+    context.write(getKey(values), new FloatWritable(Float.parseFloat(values[5].trim())));
+  }
+
+  private MovingAverageKey getKey(String[] values) {
+    String departureAirport = values[1].trim();
+    String arrivalAirport = values[2].trim();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YY");
+    Long timestamp = null;
+    try {
+      timestamp = sdf.parse(values[3].trim()).getTime();
+
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+
+    return new MovingAverageKey(departureAirport, arrivalAirport, timestamp);
   }
 
 }
