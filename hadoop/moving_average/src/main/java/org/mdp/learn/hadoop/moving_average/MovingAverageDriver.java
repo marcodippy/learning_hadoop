216a454d8f0e5be0d5852585a2beaaf673a12a8d
Marco Di Paola <dpl.marco@gmail.com>
2015-10-23 17:19:25 +0100

    use jobControl

--- hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageDriver.java
+++ hadoop/moving_average/src/main/java/org/mdp/learn/hadoop/moving_average/MovingAverageDriver.java
@@ -1,11 +1,15 @@
 package org.mdp.learn.hadoop.moving_average;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -19,16 +23,38 @@ public class MovingAverageDriver extends Configured implements Tool {
     HdfsUtils.deleteIfExists(getConf(), new Path(args[1]));
     HdfsUtils.deleteIfExists(getConf(), new Path(TEMP_OUTPUT_FILE_PATH));
 
+    JobControl jobControl = new JobControl("MOVING AVERAGE JOB CONTROL");
+
     Job averagePerDayJob = getAveragePerDayJob(args);
+    Job movingAverageJob = getMovingAverageJob(args);
 
-    boolean isJobSuccessful = averagePerDayJob.waitForCompletion(true);
+    ControlledJob averagePerDayControlledJob = new ControlledJob(averagePerDayJob, Collections.<ControlledJob> emptyList());
+    ControlledJob movingAverageControlledJob = new ControlledJob(movingAverageJob, Arrays.asList(averagePerDayControlledJob));
 
-    if (isJobSuccessful) {
-      Job movingAverageJob = getMovingAverageJob(args);
-      isJobSuccessful = movingAverageJob.waitForCompletion(true);
+    jobControl.addJob(averagePerDayControlledJob);
+    jobControl.addJob(movingAverageControlledJob);
+
+    Thread workflowThread = new Thread(jobControl, "Workflow-Thread");
+    workflowThread.setDaemon(true);
+    workflowThread.start();
+
+    while (!jobControl.allFinished()) {
+
     }
 
-    return isJobSuccessful ? 0 : 1;
+    if (!jobControl.getFailedJobList().isEmpty()) {
+      System.out.println("There are " + jobControl.getFailedJobList().size() + " job failed!");
+
+      for (ControlledJob cj : jobControl.getFailedJobList()) {
+        System.out.println(cj.getJobName() + " failed");
+      }
+
+      return 1;
+    }
+
+    System.out.println("Everything went fine, " + jobControl.getSuccessfulJobList().size() + " jobs successfully executed!");
+
+    return 0;
   }
 
   private Job getAveragePerDayJob(String[] args) throws IllegalArgumentException, IOException {
@@ -75,7 +101,7 @@ public class MovingAverageDriver extends Configured implements Tool {
     job.setMapOutputValueClass(TimeSeriesData.class);
 
     job.getConfiguration().set("mapreduce.output.textoutputformat.separator", ";");
-    
+
     job.getConfiguration().set("WindowSize", "3");
 
     job.getConfiguration().set("departureAirport_index", "0");
@@ -84,7 +110,7 @@ public class MovingAverageDriver extends Configured implements Tool {
     job.getConfiguration().set("price_index", "3");
 
     job.setNumReduceTasks(3);
-    
+
     return job;
   }
 
