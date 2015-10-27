3eb25f3eed6575f25f712fa2188972ebd34d2af0
Marco Di Paola <dpl.marco@gmail.com>
2015-10-27 17:35:15 +0000

    add crappy test on order inversion with pairs

--- hadoop/order_inversion/src/main/java/org/mdp/learn/hadoop/order_inversion/pairs/CoOccurrenceMatrixReducerWithPairs.java
+++ hadoop/order_inversion/src/main/java/org/mdp/learn/hadoop/order_inversion/pairs/CoOccurrenceMatrixReducerWithPairs.java
@@ -11,25 +11,26 @@ import org.mdp.learn.hadoop.commons.TextPair;
 public class CoOccurrenceMatrixReducerWithPairs extends Reducer<TextPair, IntWritable, TextPair, DoubleWritable> {
   private static final Text STAR     = new Text("*");
   private Integer           marginal = 0;
+  private Text              previousWord;
 
   @Override
   protected void reduce(TextPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
     if (key.getRight().equals(STAR)) {
-      for (IntWritable cnt : values) {
+      if (!key.getLeft().equals(previousWord)) marginal = 0;
+
+      for (IntWritable cnt : values)
         marginal += cnt.get();
-      }
     }
     else {
       int neighborCount = 0;
 
-      for (IntWritable cnt : values) {
+      for (IntWritable cnt : values)
         neighborCount += cnt.get();
-      }
 
       context.write(key, new DoubleWritable((double) neighborCount / marginal));
-      marginal = 0;
     }
 
+    previousWord = key.getLeft();
   }
 
 }
--- hadoop/order_inversion/src/test/java/org/mdp/learn/hadoop/order_inversion/pairs/CoOccurrenceMatrixWithPairsTest.java
+++ hadoop/order_inversion/src/test/java/org/mdp/learn/hadoop/order_inversion/pairs/CoOccurrenceMatrixWithPairsTest.java
@@ -1,6 +1,8 @@
 package org.mdp.learn.hadoop.order_inversion.pairs;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
@@ -79,55 +81,119 @@ public class CoOccurrenceMatrixWithPairsTest {
     mapDriver.runTest(false);
   }
 
-  /*
   @Test
   public void testReducer() throws IOException {
-    List<IntWritable> oneList = Arrays.asList(ONE);
+    List<IntWritable> ONE_LIST = Arrays.asList(ONE);
 
-    reducerDriver.withInput(new TextPair("this", "is"), oneList).withInput(new TextPair("this", "a"), oneList).withInput(new TextPair("this", "test"), oneList)
-        .withInput(new TextPair("this", "sentence"), Arrays.asList(ONE, ONE)).withInput(new TextPair("this", "contains"), oneList)
-        .withInput(new TextPair("is", "this"), oneList).withInput(new TextPair("is", "a"), oneList).withInput(new TextPair("is", "test"), oneList)
-        .withInput(new TextPair("a", "this"), oneList).withInput(new TextPair("a", "is"), oneList).withInput(new TextPair("a", "test"), oneList)
-        .withInput(new TextPair("a", "sentence"), oneList).withInput(new TextPair("test", "is"), oneList).withInput(new TextPair("test", "a"), oneList)
-        .withInput(new TextPair("test", "sentence"), oneList).withInput(new TextPair("test", "this"), oneList).withInput(new TextPair("sentence", "a"), oneList)
-        .withInput(new TextPair("sentence", "test"), oneList).withInput(new TextPair("sentence", "this"), Arrays.asList(ONE, ONE))
-        .withInput(new TextPair("sentence", "sentence"), Arrays.asList(ONE, ONE)).withInput(new TextPair("sentence", "contains"), oneList)
-        .withInput(new TextPair("sentence", "words"), oneList).withInput(new TextPair("contains", "this"), oneList)
-        .withInput(new TextPair("contains", "sentence"), oneList).withInput(new TextPair("contains", "words"), oneList)
-        .withInput(new TextPair("words", "sentence"), oneList).withInput(new TextPair("words", "contains"), oneList);
-
-    reducerDriver.withOutput(new TextPair("this", "is"), ONE).withOutput(new TextPair("this", "a"), ONE).withOutput(new TextPair("this", "test"), ONE)
-        .withOutput(new TextPair("this", "sentence"), new IntWritable(2)).withOutput(new TextPair("this", "contains"), ONE)
-        .withOutput(new TextPair("is", "this"), ONE).withOutput(new TextPair("is", "a"), ONE).withOutput(new TextPair("is", "test"), ONE)
-        .withOutput(new TextPair("a", "this"), ONE).withOutput(new TextPair("a", "is"), ONE).withOutput(new TextPair("a", "test"), ONE)
-        .withOutput(new TextPair("a", "sentence"), ONE).withOutput(new TextPair("test", "is"), ONE).withOutput(new TextPair("test", "a"), ONE)
-        .withOutput(new TextPair("test", "sentence"), ONE).withOutput(new TextPair("test", "this"), ONE).withOutput(new TextPair("sentence", "a"), ONE)
-        .withOutput(new TextPair("sentence", "test"), ONE).withOutput(new TextPair("sentence", "sentence"), new IntWritable(2))
-        .withOutput(new TextPair("sentence", "this"), new IntWritable(2)).withOutput(new TextPair("sentence", "contains"), ONE)
-        .withOutput(new TextPair("sentence", "words"), ONE).withOutput(new TextPair("contains", "this"), ONE)
-        .withOutput(new TextPair("contains", "sentence"), ONE).withOutput(new TextPair("contains", "words"), ONE)
-        .withOutput(new TextPair("words", "sentence"), ONE).withOutput(new TextPair("words", "contains"), ONE);
-
-    reducerDriver.runTest(false);
+    reducerDriver
+    .withInput(new TextPair("a", "*"), Arrays.asList(new IntWritable(4)))
+      .withInput(new TextPair("a", "this"), ONE_LIST)
+      .withInput(new TextPair("a", "is"), ONE_LIST)
+      .withInput(new TextPair("a", "test"), ONE_LIST)
+      .withInput(new TextPair("a", "sentence"), ONE_LIST)
+      
+    .withInput(new TextPair("contains", "*"), Arrays.asList(new IntWritable(3)))  
+      .withInput(new TextPair("contains", "this"), ONE_LIST)
+      .withInput(new TextPair("contains", "sentence"), ONE_LIST)
+      .withInput(new TextPair("contains", "words"), ONE_LIST)
+      
+    .withInput(new TextPair("is", "*"), Arrays.asList(new IntWritable(3)))
+      .withInput(new TextPair("is", "this"), ONE_LIST)
+      .withInput(new TextPair("is", "a"), ONE_LIST)
+      .withInput(new TextPair("is", "test"), ONE_LIST)
+      
+    .withInput(new TextPair("sentence", "*"), Arrays.asList(new IntWritable(4)))
+    .withInput(new TextPair("sentence", "*"), Arrays.asList(new IntWritable(4)))    
+      .withInput(new TextPair("sentence", "a"), ONE_LIST)
+      .withInput(new TextPair("sentence", "test"), ONE_LIST)
+      .withInput(new TextPair("sentence", "this"), Arrays.asList(ONE, ONE))
+      .withInput(new TextPair("sentence", "sentence"), Arrays.asList(ONE, ONE))
+      .withInput(new TextPair("sentence", "contains"), ONE_LIST)
+      .withInput(new TextPair("sentence", "words"), ONE_LIST)
+      
+    .withInput(new TextPair("test", "*"), Arrays.asList(new IntWritable(4)))
+      .withInput(new TextPair("test", "is"), ONE_LIST)
+      .withInput(new TextPair("test", "a"), ONE_LIST)
+      .withInput(new TextPair("test", "sentence"), ONE_LIST)
+      .withInput(new TextPair("test", "this"), ONE_LIST)
+      
+    .withInput(new TextPair("this", "*"), Arrays.asList(new IntWritable(2)))
+    .withInput(new TextPair("this", "*"), Arrays.asList(new IntWritable(4)))
+      .withInput(new TextPair("this", "is"), ONE_LIST)
+      .withInput(new TextPair("this", "a"), ONE_LIST)
+      .withInput(new TextPair("this", "test"), ONE_LIST)
+      .withInput(new TextPair("this", "sentence"), Arrays.asList(ONE, ONE))
+      .withInput(new TextPair("this", "contains"), ONE_LIST)
+      
+    .withInput(new TextPair("words", "*"), Arrays.asList(new IntWritable(2)))
+      .withInput(new TextPair("words", "sentence"), ONE_LIST)
+      .withInput(new TextPair("words", "contains"), ONE_LIST);
+    
+    reducerDriver
+    .withOutput(new TextPair("a", "this"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("a", "is"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("a", "test"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("a", "sentence"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("contains", "this"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("contains", "sentence"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("contains", "words"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("is", "this"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("is", "a"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("is", "test"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("sentence", "a"), new DoubleWritable(0.125))
+    .withOutput(new TextPair("sentence", "test"), new DoubleWritable(0.125))
+    .withOutput(new TextPair("sentence", "this"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("sentence", "sentence"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("sentence", "contains"), new DoubleWritable(0.125))
+    .withOutput(new TextPair("sentence", "words"), new DoubleWritable(0.125))
+    .withOutput(new TextPair("test", "is"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("test", "a"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("test", "sentence"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("test", "this"), new DoubleWritable(0.25))
+    .withOutput(new TextPair("this", "is"), new DoubleWritable(0.16666666666666666))
+    .withOutput(new TextPair("this", "a"), new DoubleWritable(0.16666666666666666))
+    .withOutput(new TextPair("this", "test"), new DoubleWritable(0.16666666666666666))
+    .withOutput(new TextPair("this", "sentence"), new DoubleWritable(0.3333333333333333))
+    .withOutput(new TextPair("this", "contains"), new DoubleWritable(0.16666666666666666))
+    .withOutput(new TextPair("words", "sentence"), new DoubleWritable(0.5))
+    .withOutput(new TextPair("words", "contains"), new DoubleWritable(0.5));
+    
+    reducerDriver.runTest();
   }
 
   @Test
   public void testMapReduce() throws IOException {
     mapRedDriver.withInput(new LongWritable(0), new Text("This is a test sentence, this sentence contains words"));
 
-    mapRedDriver.withOutput(new TextPair("this", "is"), ONE).withOutput(new TextPair("this", "a"), ONE).withOutput(new TextPair("this", "test"), ONE)
-        .withOutput(new TextPair("this", "sentence"), new IntWritable(2)).withOutput(new TextPair("this", "contains"), ONE)
-        .withOutput(new TextPair("is", "this"), ONE).withOutput(new TextPair("is", "a"), ONE).withOutput(new TextPair("is", "test"), ONE)
-        .withOutput(new TextPair("a", "this"), ONE).withOutput(new TextPair("a", "is"), ONE).withOutput(new TextPair("a", "test"), ONE)
-        .withOutput(new TextPair("a", "sentence"), ONE).withOutput(new TextPair("test", "is"), ONE).withOutput(new TextPair("test", "a"), ONE)
-        .withOutput(new TextPair("test", "sentence"), ONE).withOutput(new TextPair("test", "this"), ONE).withOutput(new TextPair("sentence", "a"), ONE)
-        .withOutput(new TextPair("sentence", "test"), ONE).withOutput(new TextPair("sentence", "sentence"), new IntWritable(2))
-        .withOutput(new TextPair("sentence", "this"), new IntWritable(2)).withOutput(new TextPair("sentence", "contains"), ONE)
-        .withOutput(new TextPair("sentence", "words"), ONE).withOutput(new TextPair("contains", "this"), ONE)
-        .withOutput(new TextPair("contains", "sentence"), ONE).withOutput(new TextPair("contains", "words"), ONE)
-        .withOutput(new TextPair("words", "sentence"), ONE).withOutput(new TextPair("words", "contains"), ONE);
+    mapRedDriver
+      .withOutput(new TextPair("a", "this"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("a", "is"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("a", "test"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("a", "sentence"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("contains", "this"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("contains", "sentence"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("contains", "words"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("is", "this"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("is", "a"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("is", "test"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("sentence", "a"), new DoubleWritable(0.125))
+      .withOutput(new TextPair("sentence", "test"), new DoubleWritable(0.125))
+      .withOutput(new TextPair("sentence", "this"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("sentence", "sentence"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("sentence", "contains"), new DoubleWritable(0.125))
+      .withOutput(new TextPair("sentence", "words"), new DoubleWritable(0.125))
+      .withOutput(new TextPair("test", "is"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("test", "a"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("test", "sentence"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("test", "this"), new DoubleWritable(0.25))
+      .withOutput(new TextPair("this", "is"), new DoubleWritable(0.16666666666666666))
+      .withOutput(new TextPair("this", "a"), new DoubleWritable(0.16666666666666666))
+      .withOutput(new TextPair("this", "test"), new DoubleWritable(0.16666666666666666))
+      .withOutput(new TextPair("this", "sentence"), new DoubleWritable(0.3333333333333333))
+      .withOutput(new TextPair("this", "contains"), new DoubleWritable(0.16666666666666666))
+      .withOutput(new TextPair("words", "sentence"), new DoubleWritable(0.5))
+      .withOutput(new TextPair("words", "contains"), new DoubleWritable(0.5));
 
     mapRedDriver.runTest(false);
   }
-  */
 }
