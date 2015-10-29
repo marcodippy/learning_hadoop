88ecea63a6bdac7a1508ac1712f86eeeb0c62679
Marco Di Paola <dpl.marco@gmail.com>
2015-10-29 14:24:14 +0000

    add map side join

---
+++ hadoop/relational_joins/src/main/java/org/mdp/learn/hadoop/relational_joins/map_side/MsDriver.java
@@ -0,0 +1,50 @@
+package org.mdp.learn.hadoop.relational_joins.map_side;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.join.CompositeInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.mdp.learn.hadoop.commons.HdfsUtils;
+
+public class MsDriver extends Configured implements Tool {
+  private static final String SEPARATOR = ";";
+
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(getConf(), "Map Side Join");
+    job.setJarByClass(MsDriver.class);
+
+    String tableLeft = args[0];
+    String tableRight = args[1];
+    String outputFile = args[2];
+
+    job.getConfiguration().set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", SEPARATOR);
+    String joinExpression = CompositeInputFormat.compose("inner", KeyValueTextInputFormat.class, tableLeft, tableRight);
+    job.getConfiguration().set("mapred.join.expr", joinExpression);
+    job.getConfiguration().set("separator", SEPARATOR);//serve?
+
+    job.setMapperClass(MsMapper.class);
+    job.setReducerClass(Reducer.class); //serve?
+    job.setNumReduceTasks(0);
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setInputFormatClass(CompositeInputFormat.class);
+    
+    FileInputFormat.addInputPaths(job, tableLeft + "," + tableRight);
+    FileOutputFormat.setOutputPath(job, new Path(outputFile));
+
+    HdfsUtils.deleteIfExists(getConf(), new Path(outputFile));
+
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    System.exit(ToolRunner.run(new MsDriver(), args));
+  }
+}
---
+++ hadoop/relational_joins/src/main/java/org/mdp/learn/hadoop/relational_joins/map_side/MsMapper.java
@@ -0,0 +1,36 @@
+package org.mdp.learn.hadoop.relational_joins.map_side;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.join.TupleWritable;
+
+public class MsMapper extends Mapper<Text, TupleWritable, NullWritable, Text> {
+  private String SEPARATOR;
+  private Text   joinedRow = new Text();
+
+  @Override
+  protected void setup(Context context) throws IOException, InterruptedException {
+    SEPARATOR = context.getConfiguration().get("mapreduce.input.keyvaluelinerecordreader.key.value.separator");
+  }
+
+  @Override
+  protected void map(Text key, TupleWritable value, Context context) throws IOException, InterruptedException {
+    String text = key.toString() + SEPARATOR;
+
+    Iterator<Writable> iterator = value.iterator();
+    while (iterator.hasNext()) {
+      text += iterator.next().toString();
+      if (iterator.hasNext()) text += SEPARATOR;
+    }
+
+    joinedRow.set(text);
+
+    context.write(NullWritable.get(), joinedRow);
+  }
+
+}
